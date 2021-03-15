524bcb04ab0a6d336987a704ccec00e185394242
Luke Edwards <luke.edwards05@gmail.com>
2021-03-15 13:23:05 -0700

    feat(request): convert `formData() -> object` via `req.body()` call;
    - a `FormData` instance can be accessed, when wanted, via `req.body.formData()`

--- src/internal/request.ts
+++ src/internal/request.ts
@@ -1,34 +1,22 @@
-// /**
-//  * @template T
-//  * @typedef Dict<T>
-//  * @type {Record<string, T>}
-//  */
-
-// /**
-//  * Converts a (Headers|FormData|URLSearchParams) into an Object.
-//  * Is similar to `Object.fromEntries` except can collect multiple values for same key.
-//  * @param {Headers|FormData|URLSearchParams} iter
-//  * @returns {Dict<string|string[]>}
-//  */
-// export function toObject(iter) {
-// 	let key, tmp, val, out = /** @type {Dict<string|string[]>} */({});
-// 	for ([key, val] of iter) {
-// 		if ((tmp = out[key]) !== void 0) {
-// 			out[key] = [].concat(tmp, val);
-// 		} else {
-// 			out[key] = val;
-// 		}
-// 	}
-// 	return out;
-// }
+type Arrayable<T> = T | Array<T>;
+type DataObject = Record<string, Arrayable<FormDataEntryValue>>;
 
 /**
- * @TODO Cast `formData` to object again?
+ * Converts a (Headers|FormData|URLSearchParams) into an Object.
+ * Is similar to `Object.fromEntries` except can collect multiple values for same key.
  */
-export async function body<T=unknown>(req: Request, ctype: string | null): Promise<T|FormData|ArrayBuffer|string|void> {
+export function toObject(iter: Headers | FormData | URLSearchParams): DataObject {
+	let key, val, tmp: Arrayable<FormDataEntryValue>, out: DataObject = {};
+	for ([key, val] of iter) {
+    out[key] = (tmp = out[key]) !== void 0 ? ([] as FormDataEntryValue[]).concat(tmp, val) : val;
+	}
+	return out;
+}
+
+export async function body<T=unknown>(req: Request, ctype: string | null): Promise<T|ArrayBuffer|string|void> {
 	if (!req.body || !ctype) return;
 	if (!!~ctype.indexOf('application/json')) return req.json() as Promise<T>;
-	if (!!~ctype.indexOf('multipart/form-data')) return req.formData();
-	if (!!~ctype.indexOf('application/x-www-form-urlencoded')) return req.formData();
+	if (!!~ctype.indexOf('multipart/form-data')) return req.formData().then(toObject) as Promise<T>;
+	if (!!~ctype.indexOf('application/x-www-form-urlencoded')) return req.formData().then(toObject) as Promise<T>;
 	return !!~ctype.indexOf('text/') ? req.text() : req.arrayBuffer();
 }
