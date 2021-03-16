0a197068e56e3dbf9bc8119ed4047b64b3a46196
Luke Edwards <luke.edwards05@gmail.com>
2021-03-15 22:51:42 -0700

    chore(kv): rename `toJSON` parameters

--- src/kv.ts
+++ src/kv.ts
@@ -7,8 +7,8 @@ export function Database<M, I extends Record<keyof M, string> = { [P in keyof M]
 		get<K extends keyof M>(type: K, uid: I[K], format?: KV.GetOptions) {
 			return read<M[K]>(binding, $(type, uid), format);
 		},
-		put<K extends keyof M>(type: K, uid: I[K], value: M[K], isJSON = true) {
-			return write<M[K]>(binding, $(type, uid), value, isJSON);
+		put<K extends keyof M>(type: K, uid: I[K], value: M[K], toJSON = true) {
+			return write<M[K]>(binding, $(type, uid), value, toJSON);
 		},
 		del<K extends keyof M>(type: K, uid: I[K]) {
 			return remove(binding, $(type, uid));
@@ -20,8 +20,8 @@ export function read<T>(binding: KV.Namespace, key: string, format: KV.GetOption
 	return binding.get<T>(key, format).then(x => x !== void 0 ? x : false);
 }
 
-export function write<T=any>(binding: KV.Namespace, key: string, value: T, isJSON: boolean = true): Promise<boolean> {
-	return binding.put(key, (!isJSON && (typeof value === 'string' || value instanceof ArrayBuffer || value instanceof ReadableStream)) ? value : JSON.stringify(value)).then(() => true, () => false);
+export function write<T=any>(binding: KV.Namespace, key: string, value: T, toJSON?: boolean): Promise<boolean> {
+	return binding.put(key, (!toJSON && (typeof value === 'string' || value instanceof ArrayBuffer || value instanceof ReadableStream)) ? value : JSON.stringify(value)).then(() => true, () => false);
 }
 
 export function remove(binding: KV.Namespace, key: string): Promise<boolean> {
