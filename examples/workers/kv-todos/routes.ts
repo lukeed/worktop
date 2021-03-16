312aa64ab5453f07fb6ad1984bb377133cbc8aa8
Luke Edwards <luke.edwards05@gmail.com>
2021-03-16 13:10:10 -0700

    example: add "kv-todos" demo

---
+++ examples/workers/kv-todos/routes.ts
@@ -0,0 +1,71 @@
+import * as Model from './model';
+
+import type { Todo } from './model';
+import type { Handler } from 'worktop';
+
+/**
+ * GET /users/:username/todos
+ */
+export const list: Handler = async function (req, res) {
+	const { username } = req.params;
+	const data = await Model.list(username);
+	res.send(200, { data }, { 'cache-control': 'private,max-age=30' });
+}
+
+/**
+ * POST /users/:username/todos
+ */
+export const create: Handler = async function (req, res) {
+	const { username } = req.params;
+
+	// Grab input values;
+	// NOTE: assumes JSON or FormData
+	const input = await req.body<Todo>();
+	const title = input && (input.title || '').trim();
+	if (!input || !title) return res.send(422, { title: 'required' });
+
+	const result = await Model.insert(username, { ...input, title });
+
+	if (result) res.send(201, result);
+	else res.send(500, 'Error creating item');
+}
+
+/**
+ * GET /users/:username/todos/:uid
+ */
+export const show: Handler = async function (req, res) {
+	const { username, uid } = req.params;
+	const item = await Model.find(username, uid);
+
+	if (item) res.send(200, item);
+	else res.send(404, 'Missing item');
+}
+
+/**
+ * PUT /users/:username/todos/:uid
+ */
+export const update: Handler = async function (req, res) {
+	const { username } = req.params;
+
+	// Grab input values;
+	// NOTE: assumes JSON or FormData
+	const input = await req.body<Todo>();
+	const title = input && (input.title || '').trim();
+	if (!input || !title) return res.send(422, { title: 'required' });
+
+	const result = await Model.update(username, input);
+
+	if (result) res.send(201, result);
+	else res.send(500, 'Error updating item');
+}
+
+/**
+ * DELETE /users/:username/todos/:uid
+ */
+export const destroy: Handler = async function (req, res) {
+	const { username, uid } = req.params;
+	const isDone = await Model.destroy(username, uid);
+
+	if (isDone) res.send(204);
+	else res.send(500, 'Error removing item');
+}
