563f3c6a4742a13f3eb490271217b7e32b0f8dc5
Luke Edwards <luke.edwards05@gmail.com>
2021-03-21 21:33:23 -0700

    feat(cache): add `reply` and `listen` exports

--- src/cache.d.ts
+++ src/cache.d.ts
@@ -4,3 +4,18 @@ export const Cache: Cache;
 export function save(event: FetchEvent, res: Response, request?: Request | string): Response;
 export function lookup(event: FetchEvent, request?: Request | string): Promise<Response | void>;
 export function isCacheable(res: Response): boolean;
+
+export type ResponseHandler = (event: FetchEvent) => Promise<Response>;
+
+/**
+ * Attempt to `lookup` the `event.request`, otherwise run the `handler` and attempt to `save` the Response.
+ * @param {ResponseHandler} handler
+ */
+export function reply(handler: ResponseHandler): FetchHandler;
+
+/**
+ * Assign the `handler` to the "fetch" event.
+ * @note Your `handler` will be wrapped by `reply` automatically.
+ * @param {ResponseHandler} handler
+ */
+export function listen(handler: ResponseHandler): void;
