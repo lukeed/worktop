4c0c213df7f19ba3be85b0b5c5ae1ac688dfafd7
Luke Edwards <luke.edwards05@gmail.com>
2021-03-21 18:30:33 -0700

    feat(crypto): add `digest` export

---
+++ src/crypto.d.ts
@@ -0,0 +1,1 @@
+export function digest(algo: 'SHA-1' | 'SHA-256' | 'SHA-384' | 'SHA-512', message: string): Promise<string>;
--- src/crypto.ts
+++ src/crypto.ts
@@ -1,1 +1,5 @@
-//
+import { encode, toHEX } from 'worktop/utils';
+
+export function digest(algo: 'SHA-1' | 'SHA-256' | 'SHA-384' | 'SHA-512', message: string): Promise<string> {
+	return crypto.subtle.digest(algo, encode(message)).then(toHEX);
+}
