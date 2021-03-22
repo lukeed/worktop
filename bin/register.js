ae29d8ed51d84076cdfe586db6a0cb6b22c94a3e
Luke Edwards <luke.edwards05@gmail.com>
2021-03-22 15:44:34 -0700

    chore(base64): add tests & type checks

--- bin/register.js
+++ bin/register.js
@@ -2,13 +2,24 @@ const { transform } = require('./esbuild');
 
 const loadJS = require.extensions['.js'];
 
+// Browser polys for Node.js
+// TODO: Remove w/ Browser test runner
+const POLYFILLS = `
+	// worktop/cache
+	globalThis.caches = { default: {} };
+
+	// worktop/base64
+	globalThis.btoa = (x) => Buffer.from(x).toString('base64');
+	globalThis.atob = (x) => Buffer.from(x, 'base64').toString();
+`;
+
 require.extensions['.ts'] = function (Module, filename) {
 	const pitch = Module._compile.bind(Module);
 
 	Module._compile = source => {
 		const { code, warnings } = transform(source, {
-			banner: 'globalThis.caches = { default: {} };',
 			sourcefile: filename,
+			banner: POLYFILLS,
 			loader: 'ts',
 		});
 
