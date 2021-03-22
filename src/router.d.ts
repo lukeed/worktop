dafe7cffc8f6cb3cbb256b2e46c72791e0e22d9d
Luke Edwards <luke.edwards05@gmail.com>
2021-03-21 21:25:50 -0700

    break(router): remove `listen` method

--- src/router.d.ts
+++ src/router.d.ts
@@ -32,7 +32,6 @@ export declare class Router {
 	add(method: string, route: RegExp | string, handler: Handler): void;
 	find(method: string, pathname: string): Route;
 	run(event: FetchEvent): Promise<Response>;
-	listen(event: FetchEvent): void;
 	onerror(req: ServerRequest, res: ServerResponse, status?: number, error?: Error): Promisable<Response>;
 	prepare?(req: Omit<ServerRequest, 'params'>, res: ServerResponse): Promisable<void>;
 }
