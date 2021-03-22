2b7c03c1c16126f4984e95d8dca20d38f07284ab
Luke Edwards <luke.edwards05@gmail.com>
2021-03-21 18:30:46 -0700

    feat(crrypto): add named `SHA*` exports

--- src/crypto.d.ts
+++ src/crypto.d.ts
@@ -1,1 +1,6 @@
 export function digest(algo: 'SHA-1' | 'SHA-256' | 'SHA-384' | 'SHA-512', message: string): Promise<string>;
+
+export function SHA1(message: string): Promise<string>;
+export function SHA256(message: string): Promise<string>;
+export function SHA384(message: string): Promise<string>;
+export function SHA512(message: string): Promise<string>;
--- src/crypto.ts
+++ src/crypto.ts
@@ -3,3 +3,8 @@ import { encode, toHEX } from 'worktop/utils';
 export function digest(algo: 'SHA-1' | 'SHA-256' | 'SHA-384' | 'SHA-512', message: string): Promise<string> {
 	return crypto.subtle.digest(algo, encode(message)).then(toHEX);
 }
+
+export const SHA1   = /*#__PURE__*/ digest.bind(0, 'SHA-1');
+export const SHA256 = /*#__PURE__*/ digest.bind(0, 'SHA-256');
+export const SHA384 = /*#__PURE__*/ digest.bind(0, 'SHA-384');
+export const SHA512 = /*#__PURE__*/ digest.bind(0, 'SHA-512');
