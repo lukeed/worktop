21a6af9a2a2732f197d52d209544ca3fe8fb2479
Luke Edwards <luke.edwards05@gmail.com>
2021-03-25 15:03:03 -0700

    examples: update `cfw` builder;
    - now handles typescript by default~!

--- examples/workers/basic/cfw.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'worktop-demo-basic',
-  zoneid: '<YOUR ZONE ID>',
-	profile: 'personal',
-	build: require('../../cfw.setup.js'), // TODO: remove
-  routes: [
-    '<YOUR DOMAIN>/*'
-  ]
-}
