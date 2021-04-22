442b1e18fa01e128368305cdda82533e697dce19
Luke Edwards <luke.edwards05@gmail.com>
2021-04-22 16:09:59 -0700

    revert: "transform `mjs` files"
    This reverts commit f5ff4c308ae72bd151627b0dce866edf92984b84.

--- bin/register.js
+++ bin/register.js
@@ -1,5 +1,7 @@
 const { transform } = require('./esbuild');
 
+const loadJS = require.extensions['.js'];
+
 // Browser polys for Node.js
 // TODO: Remove w/ Browser test runner
 const POLYFILLS = `
@@ -11,40 +13,24 @@ const POLYFILLS = `
 	globalThis.atob = (x) => Buffer.from(x, 'base64').toString();
 `;
 
-const loadCJS = require.extensions['.js'];
+require.extensions['.ts'] = function (Module, filename) {
+	const pitch = Module._compile.bind(Module);
 
-/**
- * @param {string} extn
- * @param {import('esbuild').Loader} loader
- * @param {Partial<import('esbuild').TransformOptions>} options
- */
-function loader(extn, loader, options={}) {
-	require.extensions[extn] = function (Module, filename) {
-		// @ts-ignore
-		const pitch = Module._compile.bind(Module);
+	Module._compile = source => {
+		const { code, warnings } = transform(source, {
+			sourcefile: filename,
+			banner: POLYFILLS,
+			loader: 'ts',
+		});
 
-		// @ts-ignore
-		Module._compile = source => {
-			const { code, warnings } = transform(source, {
-				sourcefile: filename,
-				loader: loader,
-				...options,
-			});
+		warnings.forEach(msg => {
+			console.warn(`\nesbuild warning in ${filename}:`);
+			console.warn(msg.location);
+			console.warn(msg.text);
+		});
 
-			warnings.forEach(msg => {
-				console.warn(`\nesbuild warning in ${filename}:`);
-				console.warn(msg.location);
-				console.warn(msg.text);
-			});
+		return pitch(code, filename);
+	};
 
-			return pitch(code, filename);
-		};
-
-		loadCJS(Module, filename);
-	}
+	loadJS(Module, filename);
 }
-
-loader('.mjs', 'js');
-loader('.ts', 'ts', {
-	banner: POLYFILLS
-});
