76a45b7dd3cd28f947d3ab8efb489e4ca29e690a
Luke Edwards <luke.edwards05@gmail.com>
2021-04-22 09:29:34 -0700

    feat(crypto): import sign, verify, keygen methods

--- src/crypto.d.ts
+++ src/crypto.d.ts
@@ -1,6 +1,21 @@
-export function digest(algo: 'SHA-1' | 'SHA-256' | 'SHA-384' | 'SHA-512', message: string): Promise<string>;
+type SHA = 'SHA-1' | 'SHA-256' | 'SHA-384' | 'SHA-512';
+type AES = 'AES-CTR' | 'AES-CBC' | 'AES-GCM' | 'AES-KW';
+type NIST = 'P-256' | 'P-384' | 'P-521';
+
+export function digest(algorithm: SHA, message: string): Promise<string>;
 
 export function SHA1(message: string): Promise<string>;
 export function SHA256(message: string): Promise<string>;
 export function SHA384(message: string): Promise<string>;
 export function SHA512(message: string): Promise<string>;
+
+// NOTE: for importKey only
+export type Algorithms =
+	| { name: 'RSASSA-PKCS1-v1_5'; hash: SHA }
+	| { name: 'RSA-OAEP'; hash: SHA }
+	| { name: 'RSA-PSS'; hash: SHA }
+	| { name: 'ECDSA'; namedCurve: NIST }
+	| { name: 'ECDH'; namedCurve: NIST }
+	| { name: 'HMAC'; hash: SHA; length?: number }
+	| { name: AES }
+	| AES | 'PBKDF2' | 'HKDF';
--- src/crypto.ts
+++ src/crypto.ts
@@ -1,4 +1,5 @@
 import { encode, toHEX } from 'worktop/utils';
+import type { Algorithms } from 'worktop/crypto';
 
 export function digest(algo: 'SHA-1' | 'SHA-256' | 'SHA-384' | 'SHA-512', message: string): Promise<string> {
 	return crypto.subtle.digest(algo, encode(message)).then(toHEX);
@@ -8,3 +9,19 @@ export const SHA1   = /*#__PURE__*/ digest.bind(0, 'SHA-1');
 export const SHA256 = /*#__PURE__*/ digest.bind(0, 'SHA-256');
 export const SHA384 = /*#__PURE__*/ digest.bind(0, 'SHA-384');
 export const SHA512 = /*#__PURE__*/ digest.bind(0, 'SHA-512');
+
+export function keygen(secret: string, algo: Algorithms, scopes: KeyUsage[]): Promise<CryptoKey> {
+	return crypto.subtle.importKey('raw', encode(secret), algo, false, scopes);
+}
+
+export async function sign(secret: string, algo: Algorithms, msg: string): Promise<ArrayBuffer> {
+	const key = await keygen(secret, algo, ['sign']);
+	const hashname = typeof algo === 'string' ? algo : algo.name;
+	return crypto.subtle.sign(hashname, key, encode(msg));
+}
+
+export async function verify(secret: string, algo: Algorithms, signature: ArrayBuffer, payload: string): Promise<boolean> {
+	const key = await keygen(secret, algo, ['verify']);
+	const hashname = typeof algo === 'string' ? algo : algo.name;
+	return crypto.subtle.verify(hashname, key, signature, encode(payload));
+}
