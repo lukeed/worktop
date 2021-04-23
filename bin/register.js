7862deeed4bc31c25dc142a4f4a45d1cd52d7748
Luke Edwards <luke.edwards05@gmail.com>
2021-04-22 18:32:29 -0700

    chore(bin): remove `banner` approach

--- bin/register.js
+++ bin/register.js
@@ -2,16 +2,13 @@ const { transform } = require('./esbuild');
 
 const loadJS = require.extensions['.js'];
 
-// Browser polys for Node.js
-// TODO: Remove w/ Browser test runner
-const POLYFILLS = `
-	// worktop/cache
-	globalThis.caches = { default: {} };
+// @ts-ignore
+// worktop/cache
+globalThis.caches = { default: {} };
 
-	// worktop/base64
-	globalThis.btoa = (x) => Buffer.from(x).toString('base64');
-	globalThis.atob = (x) => Buffer.from(x, 'base64').toString();
-`;
+// worktop/base64
+globalThis.btoa = (x) => Buffer.from(x).toString('base64');
+globalThis.atob = (x) => Buffer.from(x, 'base64').toString();
 
 require.extensions['.ts'] = function (Module, filename) {
 	const pitch = Module._compile.bind(Module);
@@ -19,7 +16,6 @@ require.extensions['.ts'] = function (Module, filename) {
 	Module._compile = source => {
 		const { code, warnings } = transform(source, {
 			sourcefile: filename,
-			banner: POLYFILLS,
 			loader: 'ts',
 		});
 
