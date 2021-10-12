974bc6821c5faf0bcc5a4d32e7977e1cbe3a070b
Luke Edwards <luke.edwards05@gmail.com>
2021-10-12 00:21:16 -0700

    chore: attach `mrmime` for mime lookup

--- packages/worktop/src/kv.assets.d.ts
+++ packages/worktop/src/kv.assets.d.ts
@@ -1,4 +1,6 @@
 import type { KV } from 'worktop/kv';
 import type { Context } from 'worktop';
 
+export { mimes } from 'mrmime';
+
 export function serve(binding: KV.Namespace, request: Request|`/${string}`, context: Context): Promise<Response | void>;
