61a558fa9a544522b8b7fc1f74f3c11b62e5092f
Luke Edwards <luke.edwards05@gmail.com>
2022-01-08 09:04:37 -0800

    fix(sw): ensure `ctx.waitUntil` is forwarded;
    - in cloudflare, the `...ctx` destructure hides the `waitUntil` & `passThrough` methods

--- packages/worktop/src/sw.ts
+++ packages/worktop/src/sw.ts
@@ -3,9 +3,11 @@ import type { Context, Initializer } from 'worktop';
 // Wrap an `Initializer` and assign as "fetch" listener
 export function start<C extends Context = Context>(run: Initializer<C>): void {
 	addEventListener('fetch', event => {
-		let { request, ...ctx } = event;
+		let req = event.request;
+		// @ts-ignore read only
+		delete event.request;
 		event.respondWith(
-			run(request, ctx as any)
+			run(req, event as any)
 		);
 	});
 }
