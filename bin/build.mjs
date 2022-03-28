81ebf5d0aedfb3da7a155ead71e3e93190c8c05a
Luke Edwards <luke.edwards05@gmail.com>
2022-03-28 13:08:22 -0700

    chore: use `build.mjs` instead

--- bin/build.mjs
+++ bin/build.mjs
@@ -1,27 +1,32 @@
 import { resolve } from 'path';
-import { promises as fs } from 'fs';
 import * as bundt from 'bundt';
 
 const root = resolve('packages');
 const minify = !process.argv.includes('--dev');
-const packages = await fs.readdir(root, { withFileTypes: true }).then(list => {
-	return list.filter(x => x.isDirectory()).map(x => x.name);
-});
 
-let result: bundt.Output = {};
-async function run(mod: string) {
+/** @type {bundt.Output} */ let result = {};
+
+/** @param {string} mod */
+async function run(mod) {
 	let dir = resolve(root, mod);
 	let stats = await bundt.build(dir, { minify });
 	Object.assign(result, stats);
 }
 
-await Promise.all(
-	packages.map(run)
-);
+let timer = process.hrtime();
+
+await Promise.all([
+	run('create-worktop'),
+	run('worktop.build'),
+	run('worktop'),
+]);
+
+timer = process.hrtime(timer);
 
 console.log(
 	await bundt.report(result, {
 		cwd: root,
 		gzip: true,
+		delta: timer,
 	})
 );
