a020875333e82ee561d8172bd02d725b1315f5fc
Luke Edwards <luke.edwards05@gmail.com>
2022-03-28 12:40:53 -0700

    chore(bin): use `tsm` for test TS transform

--- bin/register.js
+++ bin/register.js
@@ -1,7 +1,5 @@
 // @ts-check
-const { transformSync } = require('esbuild');
-
-const loadJS = require.extensions['.js'];
+require('fetchy/polyfill');
 
 // @ts-ignore - worktop/cfw.cache
 globalThis.caches = { default: {} };
@@ -12,33 +10,3 @@ globalThis.crypto = require('crypto').webcrypto;
 // worktop/base64
 globalThis.btoa = (x) => Buffer.from(x, 'binary').toString('base64');
 globalThis.atob = (x) => Buffer.from(x, 'base64').toString('binary');
-
-require('fetchy/polyfill');
-
-require.extensions['.ts'] = function (Module, filename) {
-	const pitch = Module._compile.bind(Module);
-
-	Module._compile = source => {
-		const { code, warnings } = transformSync(source, {
-			loader: 'ts',
-			format: 'cjs',
-			target: 'es2019',
-			sourcemap: 'inline',
-			sourcefile: filename,
-			minifyIdentifiers: true,
-			minifySyntax: true,
-			treeShaking: true,
-			charset: 'utf8',
-		});
-
-		warnings.forEach(msg => {
-			console.warn(`\nesbuild warning in ${filename}:`);
-			console.warn(msg.location);
-			console.warn(msg.text);
-		});
-
-		return pitch(code, filename);
-	};
-
-	loadJS(Module, filename);
-}
